c211dd2cb3d692e771e28e475af6a2a57fa87621
zinfu1111 <zinfu1111@gmail.com>
2019-04-24 14:32:14 +0800

    test6

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def handle_message(event):
         message = Confirm_Template()
         line_bot_api.reply_message(event.reply_token, message)
     elif '旋轉木馬' in msg:
-        message = Carousel_Template()
+        message = Toreba_Carousel_Template()
         line_bot_api.reply_message(event.reply_token, message)
     elif '圖片畫廊' in msg:
         message = test()
